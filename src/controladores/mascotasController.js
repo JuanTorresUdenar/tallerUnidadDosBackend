1986612aec7fe95e2ae862eb6584a18a3f48791e
Juan Carlos Torres <juancarlostorresgutierrez5@gmail.com>
2024-09-24 15:33:26 -0500

    Ya se puede buscar todas las mascotas y por id. Backend

--- src/controladores/mascotasController.js
+++ src/controladores/mascotasController.js
@@ -16,4 +16,29 @@ const crearMascota = async (req, res) => {
     }
 };
 
-export {crearMascota}
+// Buscar todas las mascotas
+const buscarMascotas = async (req, res) => {
+    try {
+        const listaMascotas = await mascotas.findAll();
+        res.status(200).json(listaMascotas);
+    } catch (error) {
+        res.status(500).json({ mensaje: `Error al buscar mascotas: ${error}` });
+    }
+};
+
+// Buscar mascota por id
+const buscarMascotaPorId = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const mascota = await mascotas.findByPk(id);
+        if (!mascota) {
+            return res.status(404).json({ mensaje: "Mascota no encontrada." });
+        }
+        res.status(200).json(mascota);
+    } catch (error) {
+        res.status(500).json({ mensaje: `Error al buscar mascota: ${error}` });
+    }
+};
+
+export {crearMascota, buscarMascotas, buscarMascotaPorId}
--- src/rutas/mascotasRouter.js
+++ src/rutas/mascotasRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { crearMascota} from "../controladores/mascotasController.js";
+import { crearMascota, buscarMascotas, buscarMascotaPorId} from "../controladores/mascotasController.js";
 
 const routerMascota = express.Router();
 
 routerMascota.post('/crear', crearMascota);
+routerMascota.get('/buscarTodo', buscarMascotas);
+routerMascota.get('/buscar/:id', buscarMascotaPorId);
 
 
 

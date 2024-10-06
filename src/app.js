67b3448e330b8f09091f31eb6e892b19f0abfbb7
Juan Carlos Torres <juancarlostorresgutierrez5@gmail.com>
2024-10-06 17:49:22 -0500

    Ultima modificacion al BackEnd, para comunicacio fluida con el frotEnd

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 import express from "express";
+import cors from "cors"; // Importa cors
 import { routerMascota } from "./rutas/mascotasRouter.js";
 import { routerSolicitud } from "./rutas/solicitarMascotaRouter.js";
-import {db} from "./database/conexion.js";
+import { db } from "./database/conexion.js";
 
-
-//Creamos la instancia de express
+// Creamos la instancia de express
 const app = express();
+
+// Configurar CORS para permitir solicitudes desde el frontend (localhost:4200)
+app.use(cors({
+    origin: 'http://localhost:4200', // Permitir solicitudes desde este origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'] // Encabezados permitidos
+}));
 
 // Verificar conexión a la base de datos 
 db.authenticate().then(() => {
-    console.log(`Conexión a base de datos correcta`);
+    console.log("Conexión a base de datos correcta");
 }).catch(err => {
-    console.log(`Error en conexión a base de datos ${err}`);
+    console.log(`Error en conexión a base de datos: ${err}`);
 });
 
 // Definir Rutas
@@ -25,14 +32,14 @@ app.use(express.json());
 app.use('/mascotas', routerMascota);
 app.use('/solicitudes', routerSolicitud);
 
-//Definimos el puerto para el que va a escuchar este servicio
-const PORT=4000;
+// Definimos el puerto para el que va a escuchar este servicio
+const PORT = 4000;
 
-
-//Abre e inicia el servicio
+// Abre e inicia el servicio
 db.sync().then(() => {
     app.listen(PORT, () => {
         console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
 }).catch(err => console.error(`Error: ${err}`));
+
 
